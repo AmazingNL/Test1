782349563f7caf0dc49ded04351e0229db3f120a
amazingnl <726138@student.inholland.nl>
2025-02-11 16:22:50 +0100

    My initial c# files committed

---
+++ IStreamable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_flix
+{
+    internal interface IStreamable
+    {
+        void StartStream();
+        void EndStream();
+    }
+}
---
+++ TVEpisode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_flix
+{
+    internal class TVEpisode : Video
+    {
+        public TVEpisode(string title, int seasonNumber, int episodNumber, int id, string genre, int duration) : base(id, title, genre, duration)
+        {
+            SeasonNumber = seasonNumber;
+            EpisodeNumber = episodNumber;
+        }
+
+        public int SeasonNumber { get;}
+        public int EpisodeNumber { get;}
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"[Movie] Title: {Title}, Genre: {Genre}, Duration: {Duration}, Season: {SeasonNumber}, EpisodeNumber: {EpisodeNumber}, ID: {Id}");
+        }
+    }
+}
