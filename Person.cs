e3e2c83c39c6c74e90ca3a7f799cc4332fa9b623
amazingnl <726138@student.inholland.nl>
2025-02-11 23:22:55 +0100

    Added some c# files

---
+++ Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_of_Custom_Objects
+{
+    internal class Person
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; } 
+        public int Age { get; set; }
+
+        public void DisplayPersonInfo()
+        {
+            Console.WriteLine($"Name: {Name} \nAge: {Age}");
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Array_of_Custom_Objects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.start();
+        }
+        void start()
+        {
+            Person[] person = new Person[3];
+            for (int i = 0; i < person.Length; i++)
+            {
+                Console.Write($"Enter a name for person {i+1}");
+                string name = Console.ReadLine();
+                Console.Write($"Enter age for person {i + 1}");
+                int age = int.Parse(Console.ReadLine());
+                person[i] = new Person(name, age);
+            }
+            
+            PrintPersonArray( person );
+            
+        }
+
+        public void PrintPersonArray(Person[] persons)
+        {
+            Console.WriteLine("Displaying all persons:");
+            for (int i = 0;i < persons.Length;i++)
+            {
+                Console.WriteLine($"Person {i+1}:");
+                persons[i].DisplayPersonInfo();
+            }
+        }
+    }
+}
